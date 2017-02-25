d29821c2e19d0146c48cf56974b241082b611eec
MynorCifuentes <mynoracifuentes09@gmail.com>
2017-02-24 20:42:59 -0600

    Practica1

---
+++ Pokemon.java
@@ -0,0 +1,41 @@
+
+public class Pokemon {
+	
+	private String nombrePokemon;
+	private String estadoPokemon;
+	private int vidaPokemon;
+	
+	public Pokemon() {
+		this.nombrePokemon = nombrePokemon;
+		this.estadoPokemon = estadoPokemon;
+		this.vidaPokemon = vidaPokemon;
+		
+	}
+
+	public String getNombrePokemon() {
+		return nombrePokemon;
+	}
+
+	public void setNombrePokemon(String nombrePokemon) {
+		this.nombrePokemon = nombrePokemon;
+	}
+
+	public String getEstadoPokemon() {
+		return estadoPokemon;
+	}
+
+	public void setEstadoPokemon(String estadoPokemon) {
+		this.estadoPokemon = estadoPokemon;
+	}
+
+	public int getVidaPokemon() {
+		return vidaPokemon;
+	}
+
+	public void setVidaPokemon(int vidaPokemon) {
+		this.vidaPokemon = vidaPokemon;
+	}
+	
+	
+
+}
