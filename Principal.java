4f2ab5afef749ca6fb0707815f2b7a1ceba51cbf
MynorCifuentes <mynoracifuentes09@gmail.com>
2017-02-24 22:38:49 -0600

    Todo por hoy

--- Principal.java
+++ Principal.java
@@ -1,4 +1,10 @@
 
 public class Principal {
+	
+	public static void main(String[] args){
+		
+		new Tamagotchi().menuPrincipal();
+	}
+	
 
 }
--- Tamagotchi.java
+++ Tamagotchi.java
@@ -6,10 +6,6 @@ public class Tamagotchi {
 	Pokemon[] mascotas;
 	
 	
-	
-	
-	
-	
 	public void menuPrincipal(){
 		entrada = new Scanner(System.in);
 		int opcion;
@@ -28,26 +24,62 @@ public class Tamagotchi {
 			
 			case 1:
 				System.out.println("Crear una Pokemon");
+				crearPokemon();
 				break;
 			case 2:
 				System.out.println("Seleccionar un Pokmeon");
+				seleccionarPokemon();
 				break;
 			case 3:
 				System.out.println("Matar Pokemon");
+				matarPokemon();
 				break;
 			case 4:
 				System.out.println("Revivir Pokemon");
+				revivirPokemon();
 				break;
 			case 5: 
 				System.exit(0);
 			
-			}
-			
-			
+			}		
 			
 		}while(opcion != 5);
 		
 	}
+	
+	
+	private void crearPokemon(){
+		int actual;
+		for(actual = 0; actual < 5; actual++){
+			System.out.println("Ingrese el nombre del Pokemon");
+			Pokemon pokemonActual = new Pokemon();
+			pokemonActual.setNombrePokemon(this.entrada.next());
+			
+			
+		}
+		
+		
+	}
+	
+	
+	private void seleccionarPokemon(){
+		
+		
+	}
+	
+	
+	private void matarPokemon(){
+		
+		
+	}
+	
+	
+	private void revivirPokemon(){
+		
+		
+	}
+	
+	
 	
 	
 
